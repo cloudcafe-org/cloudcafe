ebe3fb4fb20b9cde6b2341eb8c18ebb1da9b8c4e
MalekiRe <pocmalek@gmail.com>
2023-03-01 20:28:50 -0800

    Adding temporary looping messages to test launcher's wrapping of stdout

--- cloudcafe-client/src/main.rs
+++ cloudcafe-client/src/main.rs
@@ -1,3 +1,10 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     println!("Hello, world!");
+    for i in 0..100 {
+        println!("hi mom!");
+        thread::sleep(Duration::from_secs(1));
+    }
 }
